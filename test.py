2782fbc36dbf15ec9404ccd4b8d31baf32475e6e
guanhuichen <guanhuichen@gmail.com>
2020-06-11 23:01:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,9 +10,11 @@ def main(args):
     parser.add_argument("--minus", type=str, required=True)
     args = parser.parse_args(args)
 
+    Path(args.sum).parent.mkdir(parents=True, exist_ok=True)
     with open(args.sum, 'w') as sum_path:
         sum_path.write('{}'.format(args.arg1 + args.arg2))
         
+    Path(args.minus).parent.mkdir(parents=True, exist_ok=True)
     with open(args.minus, 'w') as minus_path:
         minus_path.write('{}'.format(args.arg1 - args.arg2))
 
